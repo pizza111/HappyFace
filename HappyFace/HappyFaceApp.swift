ed4958e596941979a7c464f6c465610edcb99a7c
Piotr Woźniak <piotr.wozniak.career@gmail.com>
2023-08-04 18:25:29 +0200

    Initial Commit

---
+++ HappyFace/HappyFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyFaceApp.swift
+//  HappyFace
+//
+//  Created by Piotr Woźniak on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HappyFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
