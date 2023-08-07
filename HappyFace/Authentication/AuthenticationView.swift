a450d7107fa4b8c371962977f0bdfe8d0855fed7
Piotr Woźniak <piotr.wozniak.career@gmail.com>
2023-08-07 20:40:19 +0200

    implementing parent view for SignInEmailView

---
+++ HappyFace/Authentication/AuthenticationView.swift
@@ -0,0 +1,38 @@
+//
+//  AuthenticationView.swift
+//  HappyFace
+//
+//  Created by Piotr Woźniak on 07/08/2023.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        VStack {
+            NavigationLink {
+                SignInEmailView()
+            } label: {
+                Text("Sign In With Email")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(.blue)
+                    .cornerRadius(10)
+            }
+            
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Sign In")
+    }
+}
+
+struct AuthenticationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            AuthenticationView()
+        }
+    }
+}
