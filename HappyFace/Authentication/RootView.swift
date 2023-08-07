9ea500ef996415a73a69ef07cbed56dbd55600bc
Piotr Woźniak <piotr.wozniak.career@gmail.com>
2023-08-07 20:46:59 +0200

    1. implementing root view 2. implementing "showSignInView" bool which manages and triggers one of two views ("AuthenticationView" / successful view ->"SettingsView" )

---
+++ HappyFace/Authentication/RootView.swift
@@ -0,0 +1,35 @@
+//
+//  RootView.swift
+//  HappyFace
+//
+//  Created by Piotr Woźniak on 07/08/2023.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    @State private var showSignInView: Bool = false
+    
+    var body: some View {
+        ZStack {
+            NavigationStack {
+                SettingsView(showSigningView: $showSignInView)
+            }
+        }
+        .onAppear {
+            let authUser = try? AuthenticationManager.shared.getAuthenticatedUser()
+            showSignInView = authUser == nil
+        }
+        .fullScreenCover(isPresented: $showSignInView) {
+            NavigationStack {
+                AuthenticationView()
+            }
+        }
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
